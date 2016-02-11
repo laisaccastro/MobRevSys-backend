11d77ad7b9e36e21302741d63f1450b869782cb9
Laísa C. de Castro <laisa.castro@hotmail.com>
2016-02-11 01:58:19 -0200

    feito alterações das classes ja existentes

--- src/main/java/com/tcc/servidor_tcc/entidades/BibFile.java
+++ src/main/java/com/tcc/servidor_tcc/entidades/BibFile.java
@@ -1,7 +1,9 @@
+
 package com.tcc.servidor_tcc.entidades;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,4 +17,66 @@ public class BibFile {
     private String name;
     private URL url;
     private List<Study> studies;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public List<Study> getStudies() {
+        return studies;
+    }
+
+    public void setStudies(List<Study> studies) {
+        this.studies = studies;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BibFile other = (BibFile) obj;
+        if (this.Id != other.Id) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.url, other.url)) {
+            return false;
+        }
+        if (!Objects.equals(this.studies, other.studies)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
--- src/main/java/com/tcc/servidor_tcc/entidades/ReviewerRole.java
+++ src/main/java/com/tcc/servidor_tcc/entidades/ReviewerRole.java
@@ -2,6 +2,7 @@ package com.tcc.servidor_tcc.entidades;
 
 import com.tcc.servidor_tcc.Type.RoleType;
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -15,5 +16,65 @@ public class ReviewerRole {
     private SystematicReview systematicReview;
     private Reviewer reviewer;
     private List<RoleType> roles;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public SystematicReview getSystematicReview() {
+        return systematicReview;
+    }
+
+    public void setSystematicReview(SystematicReview systematicReview) {
+        this.systematicReview = systematicReview;
+    }
+
+    public Reviewer getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(Reviewer reviewer) {
+        this.reviewer = reviewer;
+    }
+
+    public List<RoleType> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<RoleType> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ReviewerRole other = (ReviewerRole) obj;
+        if (this.Id != other.Id) {
+            return false;
+        }
+        if (!Objects.equals(this.systematicReview, other.systematicReview)) {
+            return false;
+        }
+        if (!Objects.equals(this.reviewer, other.reviewer)) {
+            return false;
+        }
+        if (!Objects.equals(this.roles, other.roles)) {
+            return false;
+        }
+        return true;
+    }
+    
     
 }
