42a08220cc11629ea5f7c7cf5349fdaed720d6f7
Laísa C. de Castro <laisa.castro@hotmail.com>
2016-02-11 01:56:17 -0200

    feito alterações das classes ja existentes

--- src/main/java/com/tcc/servidor_tcc/entidades/SystematicReview.java
+++ src/main/java/com/tcc/servidor_tcc/entidades/SystematicReview.java
@@ -1,6 +1,7 @@
 package com.tcc.servidor_tcc.entidades;
 
 import java.util.List;
+import java.util.Objects;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -16,7 +17,112 @@ public class SystematicReview {
     private String objective;
     private List<String> researchQuestions;
     private List<Criteria> criteria;
-    private List<Reviewer> participatingReviewers;  
+    private List<ReviewerRole> participatingReviewers;  
     private BibFile bib;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public Reviewer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Reviewer owner) {
+        this.owner = owner;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getObjective() {
+        return objective;
+    }
+
+    public void setObjective(String objective) {
+        this.objective = objective;
+    }
+
+    public List<String> getResearchQuestions() {
+        return researchQuestions;
+    }
+
+    public void setResearchQuestions(List<String> researchQuestions) {
+        this.researchQuestions = researchQuestions;
+    }
+
+    public List<Criteria> getCriteria() {
+        return criteria;
+    }
+
+    public void setCriteria(List<Criteria> criteria) {
+        this.criteria = criteria;
+    }
+
+    public List<ReviewerRole> getParticipatingReviewers() {
+        return participatingReviewers;
+    }
+
+    public void setParticipatingReviewers(List<ReviewerRole> participatingReviewers) {
+        this.participatingReviewers = participatingReviewers;
+    }
+
+    public BibFile getBib() {
+        return bib;
+    }
+
+    public void setBib(BibFile bib) {
+        this.bib = bib;
+    }
+
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SystematicReview other = (SystematicReview) obj;
+        if (this.Id != other.Id) {
+            return false;
+        }
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.objective, other.objective)) {
+            return false;
+        }
+        if (!Objects.equals(this.owner, other.owner)) {
+            return false;
+        }
+        if (!Objects.equals(this.researchQuestions, other.researchQuestions)) {
+            return false;
+        }
+        if (!Objects.equals(this.criteria, other.criteria)) {
+            return false;
+        }
+        if (!Objects.equals(this.participatingReviewers, other.participatingReviewers)) {
+            return false;
+        }
+        if (!Objects.equals(this.bib, other.bib)) {
+            return false;
+        }
+        return true;
+    }
     
 }
